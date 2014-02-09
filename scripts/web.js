2224979d5df0668acb7aecec7657d499cbbf8e41
Shawn Meyer <sgmeyer@gmail.com>
2014-02-09 15:55:50 -0600

    Cleaning up angular seed scripts
    I plan on rolling our own.  I am not using the existing stuff today so
    I just removed it.

--- scripts/web.js
+++ scripts/web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.redirect('/app/index');
+  res.redirect('/app/index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
