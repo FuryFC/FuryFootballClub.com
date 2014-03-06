da13f8e86123973e3c50057dbb5695cfdd73a99e
Shawn Meyer <sgmeyer@gmail.com>
2014-03-06 04:09:04 -0600

    Adding less support
    - Started migrating css to less files (not less syntax)
    - Added less transpiling to build process
    - Cleaned up Jade templates to kill bootstrap direct css reference

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,6 @@ module.exports = function(grunt) {
           { 'dist/app/public/js/libs/angular/angular.min.js': 'app/bower_components/angular/angular.min.js' },
           { 'dist/app/public/js/libs/jquery/jquery.min.js': 'app/bower_components/jquery/jquery.min.js' },
           { 'dist/app/public/js/libs/bootstrap/bootstrap.min.js': 'app/bower_components/bootstrap/dist/js/bootstrap.min.js' },
-          { 'dist/app/public/content/css/bootstrap.min.css': 'app/bower_components/bootstrap/dist/css/bootstrap.min.css' },
           { expand: true, cwd: 'app/bower_components/bootstrap/dist/fonts/', src: ['**'], dest: 'dist/app/public/content/font/' },
           { expand: true, cwd: 'app/img', src: ['**'], dest: 'dist/app/public/content/img/' },
           { expand: true, cwd: 'app/fonts', src: ['**'], dest: 'dist/app/public/content/font/' },
@@ -105,11 +104,19 @@ module.exports = function(grunt) {
         }
       }
     },
-    cssmin: {
-      combine: {
-        files: {
-          'dist/app/public/content/css/fury.min.css': ['app/css/**/*.css']
-        }
+    less: {
+      options: {
+        ieCompat: true,
+        paths:  ['app/css/']
+      },
+      dev: {
+        cleancss: true,
+        files: { 'dist/app/public/content/css/fury.min.css': 'app/css/site.less'},
+      },
+      prod: {
+        cleancss: true,
+        files: { 'dist/app/public/content/css/fury.min.css': 'app/css/site.less'},
+        yuicompress: true
       }
     },
     karma: {
@@ -128,9 +135,9 @@ module.exports = function(grunt) {
   grunt.registerTask("copyFiles:prod", ["copy:main", "copy:prod"]);
 
   // Build Tasks
-  grunt.registerTask("pack:dev", ["jshint", "test", "clean", "copyFiles:dev", "uglify", "cssmin"]);
-  grunt.registerTask("pack:test", ["jshint", "test", "clean", "copyFiles:test", "uglify", "cssmin"]);
-  grunt.registerTask("pack:prod", ["jshint", "test", "clean", "copyFiles:prod", "uglify", "cssmin"]);
+  grunt.registerTask("pack:dev", ["jshint", "test", "clean", "copyFiles:dev", "uglify", "less:dev"]);
+  grunt.registerTask("pack:test", ["jshint", "test", "clean", "copyFiles:test", "uglify", "less:prod"]);
+  grunt.registerTask("pack:prod", ["jshint", "test", "clean", "copyFiles:prod", "uglify", "less:prod"]);
 
   // Local Deployment Tasks
   grunt.registerTask("server", ["express", "open", "watch"]);
