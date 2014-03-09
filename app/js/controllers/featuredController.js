161c6b48be7c9350af499e2d78cc854e3a48450b
Shawn Meyer <sgmeyer@gmail.com>
2014-03-09 12:43:19 -0500

    Updating dummy data to verify repeat is working.

--- app/js/controllers/featuredController.js
+++ app/js/controllers/featuredController.js
@@ -7,37 +7,37 @@
 
     var recentNews = [
         {
-          headLine: 'Neque porro quisquam est',
+          headLine: 'Neque porro quisquam est 1',
           img: 'img/team.jpg',
           preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus, ipsum a feugiat tincidunt, arcu turpis egestas risus, quis convallis ligula tortor a turpis. Cras vel dolor in mauris blandit.',
           url: '#Test'
         },
         {
-          headLine: 'Neque porro quisquam est',
+          headLine: 'Neque porro quisquam est 2',
           img: 'img/team.jpg',
           preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus, ipsum a feugiat tincidunt, arcu turpis egestas risus, quis convallis ligula tortor a turpis. Cras vel dolor in mauris blandit.',
           url: '#Test'
         },
         {
-          headLine: 'Neque porro quisquam est',
+          headLine: 'Neque porro quisquam est 3',
           img: 'img/team.jpg',
           preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus, ipsum a feugiat tincidunt, arcu turpis egestas risus, quis convallis ligula tortor a turpis. Cras vel dolor in mauris blandit.',
           url: '#Test'
         },
         {
-          headLine: 'Neque porro quisquam est',
+          headLine: 'Neque porro quisquam est 4',
           img: 'img/team.jpg',
           preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus, ipsum a feugiat tincidunt, arcu turpis egestas risus, quis convallis ligula tortor a turpis. Cras vel dolor in mauris blandit.',
           url: '#Test'
         },
         {
-          headLine: 'Neque porro quisquam est',
+          headLine: 'Neque porro quisquam est 5',
           img: 'img/team.jpg',
           preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus, ipsum a feugiat tincidunt, arcu turpis egestas risus, quis convallis ligula tortor a turpis. Cras vel dolor in mauris blandit.',
           url: '#Test'
         },
         {
-          headLine: 'Neque porro quisquam est',
+          headLine: 'Neque porro quisquam est 6',
           img: 'img/team.jpg',
           preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus, ipsum a feugiat tincidunt, arcu turpis egestas risus, quis convallis ligula tortor a turpis. Cras vel dolor in mauris blandit.',
           url: '#Test'
